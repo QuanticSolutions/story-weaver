cd21a1de00cdbacc4ce098036b49728054ede504
QuanticSolutions <qsuidesigner@gmail.com>
2026-04-21 07:44:31 +0500

    Remove Nitro plugin from vite.config.ts
    Removed Nitro plugin from Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import { defineConfig } from 'vite';
-import { nitro } from 'nitro/vite'; // Import Nitro
 import viteReact from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [
     tanstackStart(), 
-    nitro(), // Add this!
     viteReact()
   ],
 });
